7461a33a57415a439de5bdca4fe15d0a8b852027
zamojski <andrzej.zamojski@fujitsu.com>
2026-02-27 18:28:20 +0100

    Add format of json files

---
+++ .config/nvim/ftplugin/json.lua
@@ -0,0 +1,4 @@
+vim.opt_local.expandtab = true   -- Convert tabs to spaces
+vim.opt_local.shiftwidth = 2    -- Size of an indent
+vim.opt_local.tabstop = 2       -- Number of spaces tabs count for
+vim.opt_local.softtabstop = 2   -- Number of spaces for a tab while editing
